00305db5d81f83c10d144d2185893993605213e0
chinmayi02 <58174862+chinmayi02@users.noreply.github.com>
2026-08-15 13:14:25 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        if(strs.length == 0){
+            return [[""]];
+        } else if(strs.length == 1){
+            return [strs[0]];
+        } else {
+            const hashTable = new Map();
+            for(const s of strs){
+                const sortedWord = s.split('').sort().join('');
+                if(!hashTable.has(sortedWord)){
+                    hashTable.set(sortedWord, []);
+                }
+                hashTable.get(sortedWord).push(s)
+            }
+            return Array.from(hashTable.values());
+        }
+    }
+}
