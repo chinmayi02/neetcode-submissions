1a71f82f4efb540d3feca20b57e038cee0dd5284
chinmayi02 <58174862+chinmayi02@users.noreply.github.com>
2026-09-22 08:35:00 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length){
+            return false;
+        }
+        let sortedS = s.split('').sort().join();
+        let sortedT = t.split('').sort().join();
+        if(sortedS == sortedT){
+            return true;
+        }
+        return false;
+    }
+}
